fa69ba53027dda0e3549dea9ad393662dc95d901
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-01-14 11:12:18 +0100

    Boletin 16

--- src/Boletin16/Boletin16.java
+++ src/Boletin16/Boletin16.java
@@ -5,20 +5,23 @@ package Boletin16;
 
  * @author raguiarperez
  */
-public class Exeminterfaces {
+public class Boletin16 {
 
     public static void main(String[] args) {
         //crear obxectos
        Persoa per=new Persoa();
        Canario car=new Canario();
        Galo gal=new Galo();
+       Tenor ten=new Tenor();
        //metodos
        per.cantar();
        car.cantar();
        gal.cantar();
-       IPodeCantar obx=new Persoa();
-        System.out.println("*********************");
-       obx.cantar();
+       ten.cantar();
+       //IPodeCantar obx=new Persoa();
+       //System.out.println("*********************");
+       //obx.cantar();
+
     }
     
 }
